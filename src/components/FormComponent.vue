ae6473350a47f1a0f9d85a5ca4d8d166d5162075
vovisim_home <wova53209@yandex.ru>
2025-10-07 22:49:28 +0300

    master | Фикс

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -6,22 +6,22 @@ import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/comp
 import { toTypedSchema } from '@vee-validate/zod';
 import { z } from 'zod';
 import { useForm, useFormValues } from 'vee-validate';
-import { computed, watch } from 'vue';
 import { useToast } from '@/components/ui/toast/use-toast';
 import { useLogStore } from '@/stores/useLogsStore.ts';
-import type { IItem } from '@/types/Item/IItem.ts';
 import { useDebounceFn } from '@vueuse/core';
 
 const itemsStore = useItemsStore();
 const logStore = useLogStore();
-const amount = computed(() => Number(values.value.price || 0) * Number(values.value.qty || 0)); // Считает сумму
 const { toast } = useToast();
 
 // Схема формы для валидации
 const formSchema = toTypedSchema(
   z.object({
     price: z.number({ message: 'Поле должно быть числом' }).min(1, 'Цена должна быть больше 0'),
-    qty: z.number({ message: 'Поле должно быть числом' }).min(1, 'Количество должно быть больше 0'),
+    qty: z
+      .number({ message: 'Поле должно быть числом' })
+      .int({ message: 'Количество должно быть целым числом' })
+      .min(1, 'Количество должно быть больше 0'),
     amount: z.number({ message: 'Поле должно быть числом' }).optional(),
   }),
 );
@@ -44,7 +44,6 @@ const submitValues = handleSubmit(async (values) => {
     type: 'submitPress',
     info: 'Отправить данные',
   });
-  const currentValues: Partial<IItem> = JSON.parse(JSON.stringify(values));
 
   await itemsStore.updateItem(values).then((res) => {
     if (!res) return;
